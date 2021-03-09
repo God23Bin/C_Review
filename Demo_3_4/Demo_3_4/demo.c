43fcc3ae8be75adf41138040a24af9e40ff54ea2
God23Bin <479017580@qq.com>
2021-03-09 21:14:13 +0800

    函数指针 函数指针数组的使用，转接表

--- Demo_3_4/Demo_3_4/demo.c
+++ Demo_3_4/Demo_3_4/demo.c
@@ -553,4 +553,200 @@
 
 char* my_strcpy(char* dest, const char* src);
 // 写一个函数指针pf，能够指向my_strcpy
+//char* (*pf)(char*, const char*);
 // 写一个函数指针数组，能够存放my_strcpy函数的地址
+//char* (*parr)[](char*, const char*);//你写错了，错误写法
+//char* (*parr[4])(char*, const char*);
+
+// 实现计算器
+
+//void menu() 
+//{
+//	printf("********************\n");
+//	printf("**  1.add  2.sub  **\n");
+//	printf("**  3.mul  4.div  **\n");
+//	printf("**  5.xor         **\n");
+//	printf("**     0.exit     **\n");
+//	printf("********************\n");
+//}
+//
+//int Add(int x, int y) 
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//// 加上异或
+//int XOR(int x, int y) 
+//{
+//	return x ^ y;
+//}
+
+//int main()
+//{
+//	int input = 0;
+//	int x = 0;
+//	int y = 0;
+//	// 利用函数指针数组
+//	// 通常叫转移表
+//	// 数指针数组 - 转移表
+//	int (*pfArr[])(int, int) = {0,Add,Sub,Mul,Div,XOR};// 函数指针数组
+//	do
+//	{
+//		menu();
+//		printf("请选择");
+//		scanf("%d", &input);
+//		// 利用函数指针数组的好处
+//		if (input >= 1 && input <= 5) 
+//		{
+//			printf("请输入两个操作数:>");
+//			scanf("%d%d", &x, &y);
+//			int ret = pfArr[input](x, y);
+//			printf("%d\n", ret);
+//		}
+//		else if (input == 0) 
+//		{
+//			printf("退出\n");
+//		}
+//		else 
+//		{
+//			printf("选择错误\n");
+//		}
+//		
+//	} while (input);
+//	return 0;
+//}
+
+//int main() 
+//{
+//	int input = 0;
+//	int x = 0;
+//	int y = 0;
+//	do 
+//	{
+//		menu();
+//		printf("请选择");
+//		scanf("%d", &input);
+//		switch (input) 
+//		{
+//		case 1:
+//			printf("请输入两个操作数：>");
+//			scanf("%d%d", &x, &y);
+//			printf("%d\n", Add(x, y));
+//			break;
+//		case 2:
+//			printf("请输入两个操作数：>");
+//			scanf("%d%d", &x, &y);
+//			printf("%d\n", Sub(x, y));
+//			break;
+//		case 3:
+//			printf("请输入两个操作数：>");
+//			scanf("%d%d", &x, &y);
+//			printf("%d\n", Mul(x, y));
+//			break;
+//		case 4:
+//			printf("请输入两个操作数：>");
+//			scanf("%d%d", &x, &y);
+//			printf("%d\n", Div(x, y));
+//			break;
+//		case 0:
+//			printf("退出计算器\n");
+//			break;
+//		default:
+//			printf("选择错误啦\n");
+//		}
+//	} while (input);
+//	return 0;
+//}
+// 这里的冗余代码如何解决？
+
+// 指向函数指针数组的指针
+
+// 回调函数，就是一个通过函数指针调用的函数
+// 如果你把函数的指针作为参数传递给另一个函数
+// 当这个指针被用来调用其所指向的函数时，我们就说这个是回调函数
+
+
+void menu()
+{
+	printf("********************\n");
+	printf("**  1.add  2.sub  **\n");
+	printf("**  3.mul  4.div  **\n");
+	printf("**  5.xor         **\n");
+	printf("**     0.exit     **\n");
+	printf("********************\n");
+}
+
+int Add(int x, int y)
+{
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}
+int Div(int x, int y)
+{
+	return x / y;
+}
+// 加上异或
+int XOR(int x, int y)
+{
+	return x ^ y;
+}
+
+void Calc(int (*pf)(int, int)) 
+{
+	int x = 0;
+	int y = 0;
+	printf("请输入两个操作数：>");
+	scanf("%d%d", &x, &y);
+	printf("%d\n", pf(x, y));
+}
+
+int main() 
+{
+	int input = 0;
+	do 
+	{
+		menu();
+		printf("请选择");
+		scanf("%d", &input);
+		switch (input) 
+		{
+		case 1:
+			Calc(Add);
+			break;
+		case 2:
+			Calc(Sub);
+			break;
+		case 3:
+			Calc(Mul);
+			break;
+		case 4:
+			Calc(Div);
+			break;
+		case 0:
+			printf("退出计算器\n");
+			break;
+		default:
+			printf("选择错误啦\n");
+		}
+	} while (input);
+	return 0;
+}
+
