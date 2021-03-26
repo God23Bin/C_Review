bde85b083579f4797a3f65deaac0d0ea64b67178
God23Bin <479017580@qq.com>
2021-03-26 10:06:36 +0800

    完成回调函数，指向函数指针数组的指针

--- Demo_3_4/Demo_3_4/demo.c
+++ Demo_3_4/Demo_3_4/demo.c
@@ -677,76 +677,134 @@ char* my_strcpy(char* dest, const char* src);
 // 当这个指针被用来调用其所指向的函数时，我们就说这个是回调函数
 
 
-void menu()
+//void menu()
+//{
+//	printf("********************\n");
+//	printf("**  1.add  2.sub  **\n");
+//	printf("**  3.mul  4.div  **\n");
+//	printf("**  5.xor         **\n");
+//	printf("**     0.exit     **\n");
+//	printf("********************\n");
+//}
+//
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//// 加上异或
+//int XOR(int x, int y)
+//{
+//	return x ^ y;
+//}
+//
+//void Calc(int (*pf)(int, int)) 
+//{
+//	int x = 0;
+//	int y = 0;
+//	printf("请输入两个操作数：>");
+//	scanf("%d%d", &x, &y);
+//	printf("%d\n", pf(x, y));
+//}
+//
+//int main() 
+//{
+//	int input = 0;
+//	do 
+//	{
+//		menu();
+//		printf("请选择");
+//		scanf("%d", &input);
+//		switch (input) 
+//		{
+//		case 1:
+//			Calc(Add);
+//			break;
+//		case 2:
+//			Calc(Sub);
+//			break;
+//		case 3:
+//			Calc(Mul);
+//			break;
+//		case 4:
+//			Calc(Div);
+//			break;
+//		case 0:
+//			printf("退出计算器\n");
+//			break;
+//		default:
+//			printf("选择错误啦\n");
+//		}
+//	} while (input);
+//	return 0;
+//}
+
+//指向函数指针数组的指针
+//int main() 
+//{
+//	int arr[10] = { 0 };
+//	int(*p)[10] = &arr;		// 取数组地址
+//
+//	int(*pfArr[4])(int, int);	// pfArr是一个数组 - 函数指针数组
+//	int(*(*ppfArr)[4])(int, int) = &pfArr;
+//	// ppfArr是一个数组指针，指针指向的数组有4个元素
+//	// 指向的数组的每个元素的类型是一个函数指针 int(*)(int, int)
+//
+//	// 自己操作一遍
+//	// 函数指针
+//	int(*pf)(int, int);
+//	// 函数指针数组
+//	int(*pfArr[5])(int, int);
+//	// 指向函数指针数组的指针
+//	int(*(*ppfArr)[5])(int, int);
+//	return 0;
+//}
+
+
+// 回调函数
+
+//void print(char* str) 
+//{
+//	printf("哈哈：%s", str);
+//}
+//
+//void test(void(*pf)(char*)) 
+//{
+//	printf("测试回调函数\n");
+//	pf("NBA");
+//}
+//
+//int main() 
+//{
+//	test(print);
+//	return 0;
+//}
+
+void BubbleSort(int arr[], int sz)
 {
-	printf("********************\n");
-	printf("**  1.add  2.sub  **\n");
-	printf("**  3.mul  4.div  **\n");
-	printf("**  5.xor         **\n");
-	printf("**     0.exit     **\n");
-	printf("********************\n");
+	
 }
 
-int Add(int x, int y)
+int main()
 {
-	return x + y;
-}
-int Sub(int x, int y)
-{
-	return x - y;
-}
-int Mul(int x, int y)
-{
-	return x * y;
-}
-int Div(int x, int y)
-{
-	return x / y;
-}
-// 加上异或
-int XOR(int x, int y)
-{
-	return x ^ y;
-}
+	// 冒泡排序函数
+	// 当时只能排序整形数组
+	int arr[] = { 1,3,5,7,9,2,4,6,8,0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	BubbleSozt(arr, sz); // ....
 
-void Calc(int (*pf)(int, int)) 
-{
-	int x = 0;
-	int y = 0;
-	printf("请输入两个操作数：>");
-	scanf("%d%d", &x, &y);
-	printf("%d\n", pf(x, y));
-}
-
-int main() 
-{
-	int input = 0;
-	do 
-	{
-		menu();
-		printf("请选择");
-		scanf("%d", &input);
-		switch (input) 
-		{
-		case 1:
-			Calc(Add);
-			break;
-		case 2:
-			Calc(Sub);
-			break;
-		case 3:
-			Calc(Mul);
-			break;
-		case 4:
-			Calc(Div);
-			break;
-		case 0:
-			printf("退出计算器\n");
-			break;
-		default:
-			printf("选择错误啦\n");
-		}
-	} while (input);
+	// qsort - 可以排序任意类型的数据
 	return 0;
 }
 
