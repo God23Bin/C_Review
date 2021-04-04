1a8c5589e606c07df6d85cfce8f045478cd5bdc1
God23Bin <479017580@qq.com>
2021-04-04 08:56:24 +0800

    指针完成

--- Demo_3_26/Demo_3_26/demo.c
+++ Demo_3_26/Demo_3_26/demo.c
@@ -98,133 +98,174 @@ struct Stu
 #include <stdlib.h>
 #include <string.h>
 
-int cmp_int(const void* e1, const void* e2) 
-{
-	// 比较两个整形值的函数
-	return *(int*)e1 - *(int*)e2;
-}
+//int cmp_int(const void* e1, const void* e2) 
+//{
+//	// 比较两个整形值的函数
+//	return *(int*)e1 - *(int*)e2;
+//}
+//
+//void TestInt() 
+//{
+//	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+//
+//int cmp_float(const void* e1, const void* e2) 
+//{
+//	if (*(float*)e1 == *(float*)e2) return 0;
+//	else if (*(float*)e1 > *(float*)e2) return 1;
+//	else return -1;
+//}
+//
+//
+//void TestFloat() 
+//{
+//	float f[] = { 9.0, 8.0, 7.0, 6.0, 5.0, 4.0 };
+//	int sz = sizeof(f) / sizeof(f[0]);
+//	qsort(f, sz, sizeof(f[0]), cmp_float);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%f ", f[i]);
+//	}
+//}
+//
+//int cmp_stu_by_age(const void* e1, const void* e2) 
+//{
+//	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+//}
+//
+//int cmp_stu_by_name(const void* e1, const void* e2) 
+//{
+//	// 比较名字就是比较字符串
+//	// 字符串如何比较？
+//	// 用strcmp函数比较
+//	return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->name);
+//}
+//
+//void TestStruct()
+//{
+//	struct Stu s[3] = { {"LeBron", 36},{"Kobe", 41},{"Lisi", 25} };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//	qsort(s, sz, sizeof(s[0]), cmp_stu_by_age);
+//	// 第一个参数：待排序数组的首元素地址
+//	// 第二个参数：待排序数组的元素个数
+//	// 第三个参数：待排序数组的每个元素的大小 - 单位是字节
+//	// 第四个参数：是函数指针，比较两个元素所用函数的地址，该函数需要自己实现
+//	//				函数指针的两个参数是，待比较的两个元素的地址
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", s[i].age);
+//	}
+//}
+//
+//void Swap(char* buf1, char* buf2, int width) 
+//{
+//	int i = 0;
+//	for (i = 0; i < width; i++) 
+//	{
+//		char tmp = *buf1;
+//		*buf1 = *buf2;
+//		*buf2 = tmp;
+//		buf1++;
+//		buf2++;
+//	}
+//}
+//
+//void bubble_sort(void* base, int sz, int width, int(*cmp)(void* e1, void* e2)) 
+//{
+//	int i = 0;
+//	// 趟数
+//	for (i = 0; i < sz - 1; i++) 
+//	{
+//		// 每一趟比较的次数
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i; j++) 
+//		{
+//			// 两个元素的比较
+//			if (cmp((char*)base + j * width , (char*)base + (j + 1) * width) > 0) 
+//			{
+//				// 交换
+//				Swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+//			}
+//		}
+//	}
+//}
+//
+//void bubble_sort_test_int()
+//{
+//	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	// 使用该函数的程序员，一定知道自己排序的是什么数据
+//	// 就应该知道如何比较待排序数组的元素
+//	bubble_sort(arr, sz, sizeof(arr[0]), cmp_int);
+//}
+//
+//void bubble_sort_test_struct()
+//{
+//	struct Stu s[3] = { {"LeBron", 36},{"Kobe", 41},{"Lisi", 25} };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//	//bubble_sort(s, sz, sizeof(s[0]), cmp_stu_by_name);
+//	bubble_sort(s, sz, sizeof(s[0]), cmp_stu_by_age);
+//}
+//
+//int main() 
+//{
+//	
+//	// 冒泡排序只能排整形
+//	//TestInt();
+//	//TestFloat();
+//	//TestStruct();
+//	//bubble_sort_test_int();
+//	bubble_sort_test_struct();
+//	
+//	return 0;
+//}
 
-void TestInt() 
-{
-	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	qsort(arr, sz, sizeof(arr[0]), cmp_int);
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-}
-
-int cmp_float(const void* e1, const void* e2) 
-{
-	if (*(float*)e1 == *(float*)e2) return 0;
-	else if (*(float*)e1 > *(float*)e2) return 1;
-	else return -1;
-}
-
-
-void TestFloat() 
-{
-	float f[] = { 9.0, 8.0, 7.0, 6.0, 5.0, 4.0 };
-	int sz = sizeof(f) / sizeof(f[0]);
-	qsort(f, sz, sizeof(f[0]), cmp_float);
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%f ", f[i]);
-	}
-}
-
-int cmp_stu_by_age(const void* e1, const void* e2) 
-{
-	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
-}
-
-int cmp_stu_by_name(const void* e1, const void* e2) 
-{
-	// 比较名字就是比较字符串
-	// 字符串如何比较？
-	// 用strcmp函数比较
-	return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->name);
-}
-
-void TestStruct()
-{
-	struct Stu s[3] = { {"LeBron", 36},{"Kobe", 41},{"Lisi", 25} };
-	int sz = sizeof(s) / sizeof(s[0]);
-	qsort(s, sz, sizeof(s[0]), cmp_stu_by_age);
-	// 第一个参数：待排序数组的首元素地址
-	// 第二个参数：待排序数组的元素个数
-	// 第三个参数：待排序数组的每个元素的大小 - 单位是字节
-	// 第四个参数：是函数指针，比较两个元素所用函数的地址，该函数需要自己实现
-	//				函数指针的两个参数是，待比较的两个元素的地址
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", s[i].age);
-	}
-}
-
-void Swap(char* buf1, char* buf2, int width) 
-{
-	int i = 0;
-	for (i = 0; i < width; i++) 
-	{
-		char tmp = *buf1;
-		*buf1 = *buf2;
-		*buf2 = tmp;
-		buf1++;
-		buf2++;
-	}
-}
-
-void bubble_sort(void* base, int sz, int width, int(*cmp)(void* e1, void* e2)) 
-{
-	int i = 0;
-	// 趟数
-	for (i = 0; i < sz - 1; i++) 
-	{
-		// 每一趟比较的次数
-		int j = 0;
-		for (j = 0; j < sz - 1 - i; j++) 
-		{
-			// 两个元素的比较
-			if (cmp((char*)base + j * width , (char*)base + (j + 1) * width) > 0) 
-			{
-				// 交换
-				Swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
-			}
-		}
-	}
-}
-
-void bubble_sort_test_int()
-{
-	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	// 使用该函数的程序员，一定知道自己排序的是什么数据
-	// 就应该知道如何比较待排序数组的元素
-	bubble_sort(arr, sz, sizeof(arr[0]), cmp_int);
-}
-
-void bubble_sort_test_struct()
-{
-	struct Stu s[3] = { {"LeBron", 36},{"Kobe", 41},{"Lisi", 25} };
-	int sz = sizeof(s) / sizeof(s[0]);
-	//bubble_sort(s, sz, sizeof(s[0]), cmp_stu_by_name);
-	bubble_sort(s, sz, sizeof(s[0]), cmp_stu_by_age);
-}
 
 int main() 
 {
-	
-	// 冒泡排序只能排整形
-	//TestInt();
-	//TestFloat();
-	//TestStruct();
-	//bubble_sort_test_int();
-	bubble_sort_test_struct();
-	
+	// 数组名是首元素地址
+	// 例外
+	// 1. sizeof(数组名) - 数组名代表整个数组
+	// 2. &数组名 - 数组名代表整个数组
+	// 一维数组
+	int a[] = { 1,2,3,4 };// 4 * 4 = 16
+	printf("%d\n", sizeof(a));	// 16
+	printf("%d\n", sizeof(a + 0));	// 4/8	第一个元素的地址
+	printf("%d\n", sizeof(*a));		// 4	
+	printf("%d\n", sizeof(a + 1));	// 4/8	第二个元素的地址
+	printf("%d\n", sizeof(a[1]));	// 4
+	printf("%d\n", sizeof(&a));		// 4/8	取出的是数组的地址，数组的地址也是地址，地址的大小就是4字节/8字节
+	printf("%d\n", sizeof(*&a));	// 16
+	printf("%d\n", sizeof(&a + 1));	// 4/8
+	printf("%d\n", sizeof(&a[0]));	// 4/8
+	printf("%d\n", sizeof(&a[0] + 1));// 4/8
+
+
+	// 字符数组
+	char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+	//printf("%d\n", sizeof(arr));		// 6	
+	//printf("%d\n", sizeof(arr + 0));	// 4/8
+	//printf("%d\n", sizeof(*arr));		// 1
+	//printf("%d\n", sizeof(arr[1]));		// 1
+	//printf("%d\n", sizeof(&arr));		// 4/8
+	//printf("%d\n", sizeof(&arr + 1));	// 4/8
+	//printf("%d\n", sizeof(&arr[0] + 1));//	4/8
+
+	printf("%d\n", strlen(arr));		// 随机值，遇到\0才结束	
+	printf("%d\n", strlen(arr + 0));	// 随机值
+	printf("%d\n", strlen(*arr));		// 非法访问内存了
+	printf("%d\n", strlen(arr[1]));		// 同上，err
+	printf("%d\n", strlen(&arr));		// 随机值
+	printf("%d\n", strlen(&arr + 1));	// 随机值-6
+	printf("%d\n", strlen(&arr[0] + 1));// 随机值-1
 	return 0;
 }
