9e5ab3a73c897ec504f34cb30d15ae6fc0adbe73
God23Bin <479017580@qq.com>
2021-04-04 22:42:05 +0800

    strlen()以及strcpy()的实现

---
+++ Demo_4_4/Demo_4_4/demo.c
@@ -0,0 +1,97 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+// 使用计数器
+//int my_strlen(char* str) 
+//{
+//	int count = 0;
+//	assert(str != NULL);
+//	while (*str != '\0') // while (*str)
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+
+// 使用递归
+
+// 
+
+//int main() 
+//{
+//	//int len = strlen("abcdef");
+//	//printf("%d\n", len);
+//
+//	//char arr[] = { 'a','b','c','d','e','f' };
+//	//int len1 = strlen(arr);
+//	//printf("%d\n", len1);
+//	// 
+//	// my_strlen()的实现
+//	// 1. 计数器的方法
+//	// 2. 递归
+//	// 3. 指针 - 指针
+//
+//	int len = strlen("abcdef");
+//
+//	// 下面输出什么？strlen()返回无符号整型
+//	if (strlen("abc") - strlen("abcdef") > 0) 
+//	{
+//		printf("盒盒\n");
+//	}
+//	else 
+//	{
+//		printf("哈哈\n");
+//	}
+//
+//	return 0;
+//}
+
+////char* my_strcpy(char* dest, char* src) 
+//char* my_strcpy(char* dest, const char* src) 
+//{
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//
+//	//while (*src != '\0') 
+//	//{
+//	//	*dest = *src;
+//	//	dest++;
+//	//	src++;
+//	//}
+//	//*dest = *src;	// '\0'
+//
+//	// 优化1
+//	//while (*src != '\0')
+//	//{
+//	//	*dest++ = *src++;
+//	//}
+//	//*dest = *src;	// '\0'
+//
+//	// 优化2
+//	// 拷贝src指向的字符串到dest指向的空间，包含'\0'
+//	char* rest = dest;
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	// 返回目的空间的起始地址
+//	return rest;
+//}
+//
+//int main() 
+//{
+//	char arr1[] = "abcdefghi";
+//	char arr2[] = "bit";
+//	// 把arr2的内容拷贝到arr1中
+//	//strcpy(arr1, arr2);
+//	// 怎么拷贝？
+//
+//	my_strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+
