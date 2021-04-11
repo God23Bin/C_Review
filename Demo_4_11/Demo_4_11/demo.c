5b910e2b8f99aeb06462762b25ca19bae80f5505
god23bin <479017580@qq.com>
2021-04-11 23:31:45 +0800

    结构体的各种知识

---
+++ Demo_4_11/Demo_4_11/demo.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+// 自定义类型
+
+// 结构体
+
+//char
+//short
+//int
+//float
+//long
+//long long
+//double
+
+// 结构体的声明
+//struct Stu 
+//{
+//	char ch;
+//	int age;
+//};
+//
+//  结构体的定义和初始化
+//int main() 
+//{
+//	int a = 1;
+// // 局部变量
+//	struct Stu s1 = { 'a', 12 };
+//	//s1.ch
+//	//s1.age
+//}
+
+//struct Stu
+//{
+//	char ch;
+//	int age;
+//}s1,s2,s3;
+//// 全局变量
+//
+//int main() 
+//{
+//	
+//	return 0;
+//}
+
+// 匿名结构体类型的声明
+//struct
+//{
+//	char ch;
+//	int age;
+//}s1,s2,s3;
+
+
+// 结构体的自引用
+
+//struct Node 
+//{
+//	int data;
+//	struct Node* n;
+//};
+
+//typedef struct Node
+//{
+//	int data;
+//	struct Node* n;
+//}Node;
+
